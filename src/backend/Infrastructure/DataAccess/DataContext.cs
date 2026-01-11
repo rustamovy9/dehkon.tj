dc7ecdbeb882d032268bc7ae451579626a9f33a8
rustamovy9 <yusufrustamov821@gmail.com>
2026-01-11 22:03:52 +0500

    Add new Directories and CustomCLaimTypes and DefaultRole static classes

---
+++ src/backend/Infrastructure/DataAccess/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Entities;
+using Infrastructure.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.DataAccess;
+
+public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+{
+    public DbSet<Announcement> Announcements { get; set; }
+    public DbSet<Cart> Carts { get; set; }
+    public DbSet<CartItem> CartItems { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Chat> Chats { get; set; }
+    public DbSet<ChatUser> ChatUsers { get; set; }
+    public DbSet<Message> Messages { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrdersItems { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
+        modelBuilder.FilterSoftDeletedProperties();
+    }
+}
